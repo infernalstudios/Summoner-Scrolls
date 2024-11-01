aaff940d925bb3dcbd4137fbc2b2d4a6a9cabe9f
Lyof <florian.lyof@laposte.net>
2024-11-01 16:54:32 +0100

    data reload on fabric

--- fabric/src/main/java/com/github/teamfusion/summonerscrolls/fabric/SummonerScrollsFabric.java
+++ fabric/src/main/java/com/github/teamfusion/summonerscrolls/fabric/SummonerScrollsFabric.java
@@ -1,9 +1,15 @@
 package com.github.teamfusion.summonerscrolls.fabric;
 
 import com.github.teamfusion.summonerscrolls.SummonerScrolls;
+import com.github.teamfusion.summonerscrolls.common.config.ModConfig;
 import com.github.teamfusion.summonerscrolls.common.registry.SSEvents;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.item.ItemStack;
 
@@ -15,5 +21,17 @@ public class SummonerScrollsFabric implements ModInitializer {
             return InteractionResultHolder.pass(ItemStack.EMPTY);
         });
         SummonerScrolls.commonInitialize();
+
+        ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
+            @Override
+            public ResourceLocation getFabricId() {
+                return new ResourceLocation(SummonerScrolls.MOD_ID, "reload_listener");
+            }
+
+            @Override
+            public void onResourceManagerReload(ResourceManager resourceManager) {
+                ModConfig.register();
+            }
+        });
     }
 }
