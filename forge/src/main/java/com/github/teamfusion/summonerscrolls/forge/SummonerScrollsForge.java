a6165667bb01f9e35d3f740182ace23b8dfd0c07
Lyof <florian.lyof@laposte.net>
2024-11-02 20:35:19 +0100

    data reload on forge

--- forge/src/main/java/com/github/teamfusion/summonerscrolls/forge/SummonerScrollsForge.java
+++ forge/src/main/java/com/github/teamfusion/summonerscrolls/forge/SummonerScrollsForge.java
@@ -1,7 +1,11 @@
 package com.github.teamfusion.summonerscrolls.forge;
 
 import com.github.teamfusion.summonerscrolls.SummonerScrolls;
+import com.github.teamfusion.summonerscrolls.common.config.ModConfig;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(SummonerScrolls.MOD_ID)
@@ -13,5 +17,10 @@ public class SummonerScrollsForge {
         SummonerScrolls.commonInitialize();
 
         MinecraftForge.EVENT_BUS.register(CommonEvents.class);
+        MinecraftForge.EVENT_BUS.addListener(SummonerScrollsForge::addReloadListener);
+    }
+
+    public static void addReloadListener(AddReloadListenerEvent event) {
+        event.addListener((ResourceManagerReloadListener) resourceManager -> ModConfig.register());
     }
 }
